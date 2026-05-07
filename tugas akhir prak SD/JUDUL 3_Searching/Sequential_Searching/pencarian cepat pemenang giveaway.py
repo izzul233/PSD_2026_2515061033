5ce28b392834ca114e419f9e16608c0badd8030a
izzul233 <gamer10238a@gmail.com>
2026-05-07 20:04:54 +0700

    Add files via upload

---
+++ tugas akhir prak SD/JUDUL 3_Searching/Sequential_Searching/pencarian cepat pemenang giveaway.py
@@ -0,0 +1,20 @@
+print("=== PENCARIAN CEPAT PEMENANG GIVEAWAY ===")
+
+daftar_peserta = ["Ahmad", "Budi", "Cici", "danang", "elis", "fajar"]
+nama_dicari = "supri"
+sudah_ketemu = False
+print(f"Mencari nama '{nama_dicari}' di dalam daftar...\n")
+
+for urutan in range(len(daftar_peserta)):
+    orang_yang_dicek = daftar_peserta[urutan]
+    print(f"Mengecek urutan ke-{urutan + 1}: {orang_yang_dicek}")
+    if orang_yang_dicek == nama_dicari:
+        print("\n Pemenang ditemukan")
+        print(f"Pemenang atas nama '{nama_dicari}' ada di urutan ke-{urutan + 1}.")
+        sudah_ketemu = True
+        break 
+
+if sudah_ketemu == False:
+    print(f"\nnama '{nama_dicari}' tidak ditemukan di daftar peserta.")
+
+print("Pencarian selesai.")
